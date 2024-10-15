62c3ee87417e42d6062968b56e98fb75bc169a7c
dejan-dimitrijevic92 <dejan.dimitrijevic2692@gmail.com>
2024-10-15 21:16:33 +0200

    Set up React root component

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,12 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
+    clean: true,
   },
   module: {
     rules: [
@@ -21,9 +23,18 @@ module.exports = {
       },
     ],
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      filename: 'index.html',
+    }),
+  ],
   devServer: {
-    contentBase: './dist',
+    static: {
+      directory: path.join(__dirname, 'dist'),
+    },
     hot: true,
+    port: 8080,
   },
   resolve: {
     extensions: ['.js', '.jsx'],
