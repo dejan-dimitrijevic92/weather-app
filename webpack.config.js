eece590e6dd775a0471191b6f65987fd8be987d1
dejan-dimitrijevic92 <dejan.dimitrijevic2692@gmail.com>
2024-10-17 21:59:22 +0200

    Fix env variables issue for production

--- webpack.config.js
+++ webpack.config.js
@@ -2,43 +2,47 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const Dotenv = require('dotenv-webpack');
 
-module.exports = {
-  entry: './src/index.js',
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-    clean: true,
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-        },
-      },
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
-      },
-    ],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './src/index.html',
-      filename: 'index.html',
-    }),
-    new Dotenv(),
-  ],
-  devServer: {
-    static: {
-      directory: path.join(__dirname, 'dist'),
+module.exports = (_, argv) => {
+  const isDevelopment = argv.mode === 'development';
+
+  return {
+    entry: './src/index.js',
+    output: {
+      filename: 'bundle.js',
+      path: path.resolve(__dirname, 'dist'),
+      clean: true,
     },
-    hot: true,
-    port: 8080,
-  },
-  resolve: {
-    extensions: ['.js', '.jsx'],
-  },
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+          },
+        },
+        {
+          test: /\.css$/,
+          use: ['style-loader', 'css-loader'],
+        },
+      ],
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: './src/index.html',
+        filename: 'index.html',
+      }),
+      ...(isDevelopment ? [new Dotenv()] : []),
+    ],
+    devServer: {
+      static: {
+        directory: path.join(__dirname, 'dist'),
+      },
+      hot: true,
+      port: 8080,
+    },
+    resolve: {
+      extensions: ['.js', '.jsx'],
+    },
+  };
 };
